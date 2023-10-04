88c682b4df0765591bddeeb43f309d74079ebe61
Diana Beki <dianabeki54@gmail.com>
2023-10-04 08:46:05 +0300

    Update my code with the required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
