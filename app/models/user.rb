94973285f941b3dc40d03539b6786d8f5be6fca0
iamrubydevi <ariesabyss@gmail.com>
2023-10-13 14:13:16 +0530

    Set initial posts counter to zero

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.posts_counter ||= 0
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
